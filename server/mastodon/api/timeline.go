a11f9a6feaf3d218335904e204d86939a6270356
Akihiro Urushiara <a.urusihara@gmail.com>
2023-09-04 19:06:55 +0900

    feat: public timeline template.

---
+++ server/mastodon/api/timeline.go
@@ -0,0 +1,15 @@
+package mapi
+
+import (
+	"github.com/labstack/echo/v4"
+	mopenapi "github.com/uakihir0/nostr-rest/server/mastodon/openapi"
+)
+
+func (h *MastodonHandler) GetApiV1TimelinesPublic(
+	ctx echo.Context,
+	params mopenapi.GetApiV1TimelinesPublicParams,
+) error {
+
+	// TODO implement me
+	panic("implement me")
+}
